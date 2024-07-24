a786a1481b28ac35bcab3931a63c07e2d32cbe6f
RiffVentura <riff.ventura@gmail.com>
2024-07-24 15:08:45 +0200

    added tmux config

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,23 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'christoomey/vim-tmux-navigator',
+    lazy = false,
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+    },
+    keys = {
+      { '<C-Left>', '<cmd>TmuxNavigateLeft<cr>' },
+      { '<C-Down>', '<cmd>TmuxNavigateDown<cr>' },
+      { '<C-Up>', '<cmd>TmuxNavigateUp<cr>' },
+      { '<C-Right>', '<cmd>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd>TmuxNavigatePrevious<cr>' },
+    },
+  },
+}
